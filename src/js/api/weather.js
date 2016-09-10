eeb55fd6edfcc016f96998e8a9e244b17bd04ec1
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-09-10 11:58:43 +0300

    add weather api

---
+++ src/js/api/weather.js
@@ -0,0 +1,24 @@
+import fetch from 'isomorphic-fetch';
+
+const WEATHER_API_KEY='d19be8b22d9cee2291ebb8577f647fcc'
+const WEATHER_API_URL='//api.openweathermap.org/data/2.5'
+
+
+const getCityWeather = (city='London', date=1473505200, cnt=14, units='metric') => {
+  return fetch(`${WEATHER_API_URL}/forecast/daily?q=${city}&units=${units}&cnt=${cnt}&APPID=${WEATHER_API_KEY}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      return response.json()
+    })
+    .then((response)=> {
+      const results = response.list.filter(({dt})=>dt===date)
+      if (results.length===0) {
+        throw new Error("Could not find date")
+      }
+      return results[0]
+    })
+}
+
+export default getCityWeather
