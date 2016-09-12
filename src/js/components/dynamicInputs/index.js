73ebd36061fb697b2f0ca9796fc41452b7a37855
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-09-12 12:01:20 +0300

    add components

---
+++ src/js/components/dynamicInputs/index.js
@@ -0,0 +1,61 @@
+import React, {Component, PropTypes} from 'react'
+import {connect} from 'react-redux'
+
+import {addCity,removeCity,updateCity} from '../../actions/cities'
+
+import FormControl from 'react-bootstrap/lib/FormControl'
+import ControlLabel from 'react-bootstrap/lib/ControlLabel'
+import FormGroup from 'react-bootstrap/lib/FormGroup'
+import InputGroup from 'react-bootstrap/lib/InputGroup'
+import Button from 'react-bootstrap/lib/Button'
+
+export class DynamicInputs extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const {label, cities, addCity, removeCity,updateCity} = this.props
+    return (
+      <div>
+      {
+        cities.map(({value, error},index)=> (
+          <FormGroup key={index}>
+            <ControlLabel>{label}:</ControlLabel>
+            <InputGroup>
+              <InputGroup.Button>
+                <Button onClick={()=> removeCity(index)}>-</Button>
+              </InputGroup.Button>
+              <FormControl type="text"
+                required
+                onChange={(e)=> updateCity(index,e.target.value)}
+                value={value} />
+              <InputGroup.Button>
+                <Button onClick={()=> addCity(index)}>+</Button>
+              </InputGroup.Button>
+            </InputGroup>
+          </FormGroup>
+        ))
+      }
+      </div>
+    );
+  }
+}
+DynamicInputs.propTypes = {
+  label: PropTypes.string.isRequired,
+  min: PropTypes.number,
+  max: PropTypes.number
+}
+DynamicInputs.defaultProps = {
+  min: 1,
+  max: 4
+}
+
+export const mapStateToProps = ({cities}) => {
+  return cities
+}
+export const mapDispatchToProps = (dispatch) => ({
+  addCity: (index) => dispatch(addCity(index)),
+  removeCity: (index) => dispatch(removeCity(index)),
+  updateCity: (index,value) => dispatch(updateCity(index,value))
+})
+export default connect(mapStateToProps, mapDispatchToProps)(DynamicInputs)
--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,23 +1,104 @@
 import React, {Component} from 'react'
+import ReactDOM from 'react-dom';
 import {connect} from 'react-redux'
+
+import dateFormat from 'date-format'
+import Form from 'react-bootstrap/lib/Form'
+import ControlLabel from 'react-bootstrap/lib/ControlLabel'
+import FormGroup from 'react-bootstrap/lib/FormGroup'
+import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar'
+import Button from 'react-bootstrap/lib/Button'
+import FormControl from 'react-bootstrap/lib/FormControl'
+import Panel from 'react-bootstrap/lib/Panel'
+
+import DynamicInputs from './dynamicInputs'
 import OfflineUpdate from './offline-update'
 import offlineProps from './offline-update/offline-props'
 
-require('./styles.scss')
+import {loadData} from '../actions'
+
+require('./styles/bootstrap.scss')
+require('./styles/styles.scss')
 
 export class Main extends Component {
   constructor(props) {
     super(props);
   }
+  submitForm(e) {
+    e.preventDefault()
+    const {loadData, inputCities} = this.props
+    const date = new Date(ReactDOM.findDOMNode(this.dateInput).value)
+    const fromCity = ReactDOM.findDOMNode(this.fromCityInput).value;
+    loadData(fromCity, inputCities.map(({value})=>value), date)
+  }
   render() {
+    const {loading, cities} = this.props
+    const maxDate = new Date();
+    maxDate.setDate(maxDate.getDate() + 14);
     return (
-      <div>
+      <div className="container">
         <OfflineUpdate {...offlineProps()}/>
+        <Form onSubmit={(e)=>this.submitForm(e)} className={loading && 'loading'}>
+          <FormGroup>
+            <ControlLabel>Date:</ControlLabel>
+            <FormControl type="date" ref={node => {this.dateInput = node}}
+              required
+              defaultValue={dateFormat('yyyy-MM-dd', new Date())}
+              min={dateFormat('yyyy-MM-dd', new Date())}
+              max={dateFormat('yyyy-MM-dd', maxDate)} />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>From city:</ControlLabel>
+            <FormControl
+              type="text"
+              required
+              ref={node => {this.fromCityInput = node}}
+              defaultValue="Thessaloniki" />
+          </FormGroup>
+          <DynamicInputs
+            max={4}
+            label="To city"
+            ref={node => {this.toCityInputs = node}}
+            />
+          <ButtonToolbar>
+            <Button type="reset">Reset</Button>
+            <Button type="submit" bsStyle="primary" className={loading && 'disabled'}>Submit</Button>
+          </ButtonToolbar>
+        </Form>
+        <br/>
+        {
+          cities.map(({flight, weather}, index)=> {
+            return (
+              <Panel key={index} header={`Weather & flights for ${flight.toCity}`} className="">
+                <Panel>
+                  <strong>{flight.toCity} weather:</strong> {weather.weather[0].description} <br/>
+                  <strong>Temperature:</strong>  {Math.floor(weather.temp.day)}C <br/>
+                </Panel>
+                <Panel>
+                  <strong>Sale Total:</strong>     {flight.saleTotal} <br/>
+                  <strong>From Airport:</strong>   {flight.fromAirport} <br/>
+                  <strong>To Airport:</strong>     {flight.toAirport} <br/>
+                  <strong>Carier:</strong>         {flight.carier} <br/>
+                  <strong>Departure Time:</strong> {dateFormat('MM-dd-yyyy hh:mm', new Date(flight.departureTime))} <br/>
+                  <strong>Arrival Time:</strong>   {dateFormat('MM-dd-yyyy hh:mm', new Date(flight.arrivalTime))} <br/>
+                </Panel>
+              </Panel>
+            )
+          })
+        }
       </div>
     )
   }
 }
 
-export const mapStateToProps = ({ weather } ) => weather;
+export const mapStateToProps = ({ weatherAndFlights, cities } ) => {
+  return {
+    ...weatherAndFlights,
+    inputCities: cities.cities
+  }
+};
+export const mapDispatchToProps = (dispatch) => ({
+  loadData: (fromCity, toCities, date) => dispatch(loadData(fromCity, toCities, date))
+})
 
-export default connect(mapStateToProps)(Main)
+export default connect(mapStateToProps, mapDispatchToProps)(Main)
