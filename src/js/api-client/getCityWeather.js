e3a3b7c7bf247ba20961bbb5457572814bb86ca9
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-09-27 20:49:38 +0300

    clean up

--- src/js/api-client/getCityWeather.js
+++ src/js/api-client/getCityWeather.js
@@ -2,14 +2,14 @@ import dateFormat from 'date-format'
 import fetch from 'isomorphic-fetch';
 
 const WEATHER_API_KEY='d19be8b22d9cee2291ebb8577f647fcc'
-const WEATHER_API_URL='http://api.openweathermap.org/data/2.5'
+const WEATHER_API_URL='http://api.openweathermap.org/data/2.5/forecast/daily'
 
-export function getWeatherDate(date) {
+export function getDateAsNumber(date) {
   return parseInt(dateFormat('yyyyMMdd', date), 10);
 }
 
 export const getCityWeather = (city='London', startDate=new Date(), endDate=new Date(), cnt=14, units='metric') => {
-  return fetch(`${WEATHER_API_URL}/forecast/daily?q=${city}&units=${units}&cnt=${cnt}&APPID=${WEATHER_API_KEY}`)
+  return fetch(`${WEATHER_API_URL}?q=${city}&units=${units}&cnt=${cnt}&APPID=${WEATHER_API_KEY}`)
     .then((response) => {
       if (!response.ok) {
         throw new Error(response.statusText)
@@ -18,9 +18,9 @@ export const getCityWeather = (city='London', startDate=new Date(), endDate=new 
     })
     .then((response)=> {
       const filteredWeatherList = response.list.filter(({dt})=> {
-        dt = getWeatherDate(new Date(dt*1000))
-        return (dt >= getWeatherDate(startDate) &&
-                dt <= getWeatherDate(endDate))
+        dt = getDateAsNumber(new Date(dt*1000))
+        return (dt >= getDateAsNumber(startDate) &&
+                dt <= getDateAsNumber(endDate))
       })
       if (filteredWeatherList.length===0) {
         throw new Error("Could not find weather date!")
