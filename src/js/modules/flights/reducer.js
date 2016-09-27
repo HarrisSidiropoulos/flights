3707f7113fa190d5b06cb43790b2e37a3aeb601e
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-09-27 20:53:04 +0300

    clean up

--- src/js/modules/flights/reducer.js
+++ src/js/modules/flights/reducer.js
@@ -1,36 +1,35 @@
-/*eslint indent: */
 import {RESET_DATA, FETCH_DATA_REQUEST, FETCH_DATA_FAILURE, FETCH_DATA_SUCCESS} from './actionTypes'
 export const INITIAL_STATE = { loading: false, cities: [], error: false }
 
 export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case RESET_DATA:
-      return {
-        ...INITIAL_STATE
-      }
-    case FETCH_DATA_REQUEST:
-      return {
-        ...state,
-        cities: [],
-        error: false,
-        loading: true
-      }
-    case FETCH_DATA_FAILURE:
-      return {
-        loading: false,
-        cities: [],
-        error: action.error
-      }
-    case FETCH_DATA_SUCCESS:
-      return {
-        ...state,
-        error: false,
-        loading: false,
-        cities: action.payload
-      }
-    default:
-      return {
-        ...state
-      }
+  case RESET_DATA:
+    return {
+      ...INITIAL_STATE
+    }
+  case FETCH_DATA_REQUEST:
+    return {
+      ...state,
+      cities: [],
+      error: false,
+      loading: true
+    }
+  case FETCH_DATA_FAILURE:
+    return {
+      loading: false,
+      cities: [],
+      error: action.error
+    }
+  case FETCH_DATA_SUCCESS:
+    return {
+      ...state,
+      error: false,
+      loading: false,
+      cities: action.payload
+    }
+  default:
+    return {
+      ...state
+    }
   }
 }
