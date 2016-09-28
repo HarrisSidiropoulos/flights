a5b8653f7e5bf430b4f0fb48eb815273435342b5
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-09-28 15:12:03 +0300

    add getCityWeather response json

--- src/js/api-client/getCityWeather.test.js
+++ src/js/api-client/getCityWeather.test.js
@@ -6,48 +6,8 @@ describe('getCityWeather', ()=> {
     nock.cleanAll()
   })
   it('fetch city weather', () => {
-    const expectedValue = {
-      "city":{
-        "id":264371,
-        "name":"Athens",
-        "coord":{
-          "lon":23.716221,
-          "lat":37.97945
-        },
-        "country":"GR",
-        "population":0
-      },
-      "cod":"200",
-      "message":0.0215,
-      "cnt":14,
-      "list":[
-        {
-          "dt":1475056800,
-          "temp":{
-            "day":23.38,
-            "min":20.79,
-            "max":23.74,
-            "night":20.79,
-            "eve":23.06,
-            "morn":23.38
-          },
-          "pressure":1033.63,
-          "humidity":79,
-          "weather":[
-            {
-              "id":800,
-              "main":"Clear",
-              "description":"clear sky",
-              "icon":"01d"
-            }
-          ],
-          "speed":9.61,
-          "deg":28,
-          "clouds":0
-        }
-      ]
-    }
-    
+    const expectedValue = require('./getCityWeather.response.json')
+
     const startDate = new Date(1475056800000)
     const endDate   = new Date(1475056800000)
     const cnt  = 1
