2a253f1135021375588bff4080af1d6f3104aa24
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-09-30 13:30:37 +0300

    add actions tests

---
+++ src/js/modules/flights/actions.test.js
@@ -0,0 +1,50 @@
+import {
+  RESET_DATA,
+  FETCH_DATA_FAILURE,
+  FETCH_DATA_SUCCESS,
+  FETCH_DATA_REQUEST
+} from './actionTypes'
+import {
+  resetData,
+  requestError,
+  receiveData,
+  requestData
+} from './actions'
+
+describe('flights actions', ()=> {
+  it('should create an action to reset all data', () => {
+    const expectedAction = {
+      type: RESET_DATA
+    }
+    expect(resetData()).toEqual(expectedAction)
+  })
+  it('should create an action to all data errors', () => {
+    const error='error'
+    const expectedAction = {
+      type: FETCH_DATA_FAILURE,
+      error
+    }
+    expect(requestError(error)).toEqual(expectedAction)
+  })
+  it('should create an action to receive data', () => {
+    const payload = 'payload'
+    const expectedAction = {
+      type: FETCH_DATA_SUCCESS,
+      payload
+    }
+    expect(receiveData(payload)).toEqual(expectedAction)
+  })
+  it('should create an action to request data', () => {
+    const fromCity = "Thessaloniki"
+    const toCities = ["Athens"]
+    const startDate = new Date()
+    const endDate = new Date()
+    const expectedAction = {
+      type: FETCH_DATA_REQUEST,
+      payload: {
+        fromCity, toCities, startDate, endDate
+      }
+    }
+    expect(requestData(fromCity, toCities, startDate, endDate)).toEqual(expectedAction)
+  })
+})
