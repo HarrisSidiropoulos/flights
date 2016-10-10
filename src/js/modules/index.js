cddf869d5ab145538160f6780f28d62ec836a42d
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-10-10 20:05:35 +0300

    clean up

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -1,11 +1,9 @@
 import Main from './Main'
-import * as DynamicInputs from './dynamic-inputs'
 import * as Flights from './flights'
 import * as OfflineUpdate from './offline-update'
 
 export {
   Main,
-  DynamicInputs,
   Flights,
   OfflineUpdate
 }
