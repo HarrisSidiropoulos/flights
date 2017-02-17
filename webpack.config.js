07a32ff41110fd18e3a6304d87324c226b343b9b
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2017-02-17 15:19:43 +0200

    eslint webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,94 @@
-/* eslint no-console: */
-const {resolve} = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const OfflinePlugin = require('offline-plugin')
-const packageJSON = require('./package.json')
-const CompressionPlugin = require("compression-webpack-plugin")
-const convert = require('object-array-converter')
+/* eslint-disable no-console, no-param-reassign, max-len, camelcase */
+const { resolve, } = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const OfflinePlugin = require('offline-plugin');
+const packageJSON = require('./package.json');
+const CompressionPlugin = require('compression-webpack-plugin');
+const convert = require('object-array-converter');
 
 module.exports = env => {
   env = env || {};
-  const rxjs = ['rxjs/Observable', 'rxjs/Subject', 'rxjs/observable/fromPromise', 'rxjs/observable/of', 'rxjs/observable/merge', 'rxjs/add/operator/mergeMap', 'rxjs/add/operator/do', 'rxjs/add/operator/debounceTime', 'rxjs/add/operator/filter', 'rxjs/add/operator/takeWhile', 'rxjs/add/operator/takeUntil', 'rxjs/add/operator/catch', 'rxjs/add/operator/map', 'rxjs/operator/switchMap']
-  const material = ['material-ui/RaisedButton','material-ui/RefreshIndicator','material-ui/Card','material-ui/TextField','material-ui/DatePicker','material-ui/FlatButton','material-ui/AutoComplete','material-ui/styles/MuiThemeProvider']
-  let dependencies = convert.toArray(packageJSON.dependencies)
-    .map(({key})=>key)
-    .filter(val=>val!=='rxjs')
-    .filter(val=>val!=='material-ui')
-    .concat(rxjs).concat(material)
-  const specifyProp = (add, value) => add ? value : undefined
-  const ifProd = value => specifyProp(env.prod, value)
-  const ifDev = value => specifyProp(!env.prod, value)
+  const rxjs = ['rxjs/Observable', 'rxjs/Subject', 'rxjs/observable/fromPromise', 'rxjs/observable/of', 'rxjs/observable/merge', 'rxjs/add/operator/mergeMap', 'rxjs/add/operator/do', 'rxjs/add/operator/debounceTime', 'rxjs/add/operator/filter', 'rxjs/add/operator/takeWhile', 'rxjs/add/operator/takeUntil', 'rxjs/add/operator/catch', 'rxjs/add/operator/map', 'rxjs/operator/switchMap',];
+  const material = ['material-ui/RaisedButton','material-ui/RefreshIndicator','material-ui/Card','material-ui/TextField','material-ui/DatePicker','material-ui/FlatButton','material-ui/AutoComplete','material-ui/styles/MuiThemeProvider',];
+  const dependencies = convert.toArray(packageJSON.dependencies)
+    .map(({ key, }) => key)
+    .filter(val => val !== 'rxjs')
+    .filter(val => val !== 'material-ui')
+    .concat(rxjs)
+    .concat(material);
+  const specifyProp = (add, value) => {
+    return add ? value : undefined;
+  };
+  const ifProd = value => specifyProp(env.prod, value);
+  const ifDev = value => specifyProp(!env.prod, value);
   const removeEmpty = obj => {
     if (Array.isArray(obj)) {
-      return obj.filter(i => !!i)
+      return obj.filter(item => !!item);
     }
-    for (var i in obj) {
-      if (obj[i] === null || obj[i] === undefined) {
-        delete obj[i];
+    for (var name in obj) {
+      if (obj[name] === null || obj[name] === undefined) {
+        delete obj[name];
       }
     }
     return obj;
-  }
-  const assetsPath = env.prod?'assets/':''
-  const indexPath = env.prod?'../':''
+  };
+  const assetsPath = env.prod ? 'assets/' : '';
+  const indexPath = env.prod ? '../' : '';
   return removeEmpty({
     entry: removeEmpty({
       vendor: ifProd(dependencies),
       app: removeEmpty([
         ifDev('webpack-hot-middleware/client?reload=true'),
-        './js/index.js'
-      ])
+        './js/index.js',
+      ]),
     }),
     output: {
       filename: env.prod ? 'bundle.[name].[chunkhash].js' : '[name].js',
       path: resolve(__dirname, `dist/${assetsPath}`),
       pathinfo: !env.prod,
-      publicPath: assetsPath
+      publicPath: assetsPath,
     },
     context: resolve(__dirname, 'src'),
     devtool: ifDev('eval-source-map'),
     module: {
       loaders: removeEmpty([
-        {test: /\.js$/, loader: 'babel', query: { "presets": removeEmpty(["es2015", "stage-2", "react", ifDev("react-hmre")]), "plugins": ["transform-class-properties"] }, exclude: /node_modules/},
-        {test: /\.jade$/, loader: 'jade'},
-        ifProd({test: /\.css$/, loader: 'style!css?modules', include: /flexboxgrid/}),
-        ifDev({test: /\.css$/,   loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'}),
-        ifDev({test: /\.scss$/,  loaders: ["style", "css", "sass?sourceMap"]}),
-        // ifProd({test: /\.css$/,   loader: ExtractTextPlugin.extract("style-loader", "css-loader")}),
-        ifProd({test: /\.scss$/,  loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")}),
-        {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff"},
-        {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"},
-        {test: /\.(jpg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=15000"},
-        {test: /\.(png|gif|mp3)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"},
-        ifProd({ test: /\.js$/, loader: "strip-loader?strip[]=console.log" })
-      ])
+        { test: /\.js$/, loader: 'babel', query: { presets: removeEmpty(['es2015', 'stage-2', 'react', ifDev('react-hmre'),]), plugins: ['transform-class-properties',], }, exclude: /node_modules/, },
+        { test: /\.jade$/, loader: 'jade', },
+        ifProd({ test: /\.css$/, loader: 'style!css?modules', include: /flexboxgrid/, }),
+        ifDev({ test: /\.css$/,   loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]', }),
+        ifDev({ test: /\.scss$/,  loaders: ['style', 'css', 'sass?sourceMap',], }),
+        // ifProd({test: /\.css$/,   loader: ExtractTextPlugin.extract('style-loader', 'css-loader')}),
+        ifProd({ test: /\.scss$/,  loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader'), }),
+        { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff', },
+        { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader', },
+        { test: /\.(jpg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=15000', },
+        { test: /\.(png|gif|mp3)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader', },
+        ifProd({ test: /\.js$/, loader: 'strip-loader?strip[]=console.log', }),
+      ]),
     },
     recordsPath: resolve(__dirname, './webpack-records.json'),
     plugins: removeEmpty([
       ifProd(new webpack.optimize.DedupePlugin()),
       ifProd(new webpack.LoaderOptionsPlugin({
         minimize: true,
-        debug: false
+        debug: false,
       })),
       ifProd(new webpack.optimize.UglifyJsPlugin({
         compress: {
-          screw_ie8:true,
-          warnings: false
-        }
+          screw_ie8 : true,
+          warnings : false,
+        },
       })),
-      ifProd(new webpack.optimize.CommonsChunkPlugin({name:'vendor'})),
+      ifProd(new webpack.optimize.CommonsChunkPlugin({ name:'vendor', })),
       ifProd(new ExtractTextPlugin('bundle.[name]-[hash].min.css')),
       ifProd(new CompressionPlugin({
-        asset: "[path].gz[query]",
-        algorithm: "gzip",
+        asset: '[path].gz[query]',
+        algorithm: 'gzip',
         test: /\.js$|\.css$/,
         threshold: 10240,
-        minRatio: 0.8
+        minRatio: 0.8,
       })),
       new HtmlWebpackPlugin({
         filename: `${indexPath}index.html`,
@@ -94,13 +97,13 @@ module.exports = env => {
         description: packageJSON.description,
         template: './index.jade',
         NODE_ENV: env.prod ? 'production' : 'development',
-        inject: !env.prod
+        inject: !env.prod,
       }),
       ifDev(new webpack.optimize.OccurrenceOrderPlugin()),
       ifDev(new webpack.HotModuleReplacementPlugin()),
       ifDev(new webpack.NoErrorsPlugin()),
-      new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify(env.prod ?'production':'development')}),
-      ifProd(new OfflinePlugin({ServiceWorker:{events:true},AppCache:false}))
-    ])
-  })
-}
+      new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(env.prod ? 'production' : 'development'), }),
+      ifProd(new OfflinePlugin({ ServiceWorker:{ events:true, }, AppCache:false, })),
+    ]),
+  });
+};
